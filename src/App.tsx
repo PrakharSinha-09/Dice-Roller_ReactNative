2c1b536a639067da8358895d23c0e13afc5dc542
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2023-09-12 20:37:23 +0530

    Added images for the dice and rolling functionality as well

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   StyleSheet,
   Text,
   View,
   Image,
-  ImageSourcePropType
+  ImageSourcePropType,
+  Button,
+  TouchableOpacity,
+  Pressable,
 } from 'react-native';
+
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
 
 //in react-native remember that your image must be resolved like a module and so is the reason I've created index.d.ts file,(the file name is important), where I've specified that the images will come as module!
 //These DiceOne till DiceSix should be interpreted like variable, where these images are being stored!... (the typescript way!)
@@ -21,6 +26,11 @@ type DiceProps=PropsWithChildren<{
   imageUrl:ImageSourcePropType
 }>
 
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
+
 const Dice=({imageUrl}: DiceProps): JSX.Element=>{
   return(
     <View>
@@ -30,10 +40,43 @@ const Dice=({imageUrl}: DiceProps): JSX.Element=>{
 }
 
 function App(): JSX.Element {
-  
+  const [diceImage,setDiceImage]=useState<ImageSourcePropType>(DiceOne)            //don't be overwhelm by that angular brackets, it is just that we are clarifying that we need only image, and not any other stuffs... simple typecscript thing, even if we remove that, it won't affect anything, but its a good practice indeed!
+
+  const rollDiceOnClick=()=>{
+    let randomNumber=Math.floor(Math.random()*6)+1
+
+    switch (randomNumber) {
+      case 1:
+        setDiceImage(DiceOne)
+        break;
+      case 2:
+        setDiceImage(DiceTwo)
+        break;
+      case 3:
+        setDiceImage(DiceThree)
+        break;
+      case 4:
+        setDiceImage(DiceFour)
+        break;
+      case 5:
+        setDiceImage(DiceFive)
+        break;
+      case 6:
+        setDiceImage(DiceSix)
+        break;
+    
+      default: setDiceImage(DiceOne)
+        break;
+    }
+
+    ReactNativeHapticFeedback.trigger("impactLight", options);
+  }
   return (
-    <View>
-      <Text>Hey There!</Text>
+    <View style={styles.container}>
+      <Dice imageUrl={diceImage} />
+      <Pressable onPress={()=>rollDiceOnClick()}>
+        <Text style={styles.rollDiceBtnText}>Click To Roll!</Text>
+      </Pressable>
     </View>
   );
 }
